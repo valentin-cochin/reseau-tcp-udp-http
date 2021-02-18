373fa4247d374d77194662b7a4a9cf5b4d7dbf99
isame el mesbahi <isame.elmesbahi@gmail.com>
2021-02-18 21:40:47 +0100

    client à utiliser avec le serveur incrementeur

--- client-app/src/main/java/com/cda/reseau/client/ClientApp.java
+++ client-app/src/main/java/com/cda/reseau/client/ClientApp.java
@@ -1,10 +1,14 @@
 package com.cda.reseau.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
+
 
 public class ClientApp {
 
@@ -15,15 +19,25 @@ public class ClientApp {
 		System.out.println("client : tentative de connexion");
 		sc.connect(isa);
 		
+		Scanner clavierIn = new Scanner(System.in);
 		InputStream is = sc.getInputStream();
-		int x = is.read();
-		
-		System.out.println("client : x = "+(char)x);
-		
 		OutputStream os = sc.getOutputStream();
-		os.write(x+1);
+		
+		// pour faciliter la lecture, passer par des buffer
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(is));
+
+		String phraseRecue = bfr.readLine();
+		System.out.print(phraseRecue);
+		
+		char caractereSaisie = clavierIn.next().charAt(0);
+
+		os.write(caractereSaisie);
+		bfr.readLine();
+		phraseRecue = bfr.readLine();
+		System.out.println(phraseRecue);
 		
 		sc.close();
+		clavierIn.close();
 		
 	}
 
