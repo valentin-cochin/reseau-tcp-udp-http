d01da2d8361cf968029a76714ecf2ea17f8b0ce3
isame el mesbahi <isame.elmesbahi@gmail.com>
2021-02-18 20:45:36 +0100

    serveur à tester avec telnet

--- serveur-app/src/main/java/com/cda/reseau/serveur/ServeurApp.java
+++ serveur-app/src/main/java/com/cda/reseau/serveur/ServeurApp.java
@@ -1,9 +1,30 @@
 package com.cda.reseau.serveur;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServeurApp {
-
-	public static void main(String[] args) {
-
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(8080);
+		
+		System.out.println("serveur : en attente de connexion ...");
+		Socket sc = ss.accept();
+		
+		System.out.println("serveur : connexion établie !");
+		OutputStream os = sc.getOutputStream();
+		
+		os.write(99);
+		
+		InputStream is = sc.getInputStream();
+		int y = is.read();
+		
+		System.out.println("serveur : y : "+(char)y);
+		
+		ss.close();
 	}
 
 }
