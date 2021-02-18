64a06e52bc0e9c7ea02fcca3323f20c5b26087b8
isame el mesbahi <isame.elmesbahi@gmail.com>
2021-02-18 20:30:42 +0100

    mise en place de la structure

---
+++ client-app/src/main/java/com/cda/reseau/client/ClientApp.java
@@ -0,0 +1,9 @@
+package com.cda.reseau.client;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ serveur-app/src/main/java/com/cda/reseau/serveur/ServeurApp.java
@@ -0,0 +1,9 @@
+package com.cda.reseau.serveur;
+
+public class ServeurApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
