6a67475087f361cd29d5304368e26464fb7497f3
isame el mesbahi <isame.elmesbahi@gmail.com>
2021-02-18 21:03:14 +0100

    client à utiliser avec le serveur

--- client-app/src/main/java/com/cda/reseau/client/ClientApp.java
+++ client-app/src/main/java/com/cda/reseau/client/ClientApp.java
@@ -1,9 +1,30 @@
 package com.cda.reseau.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class ClientApp {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		Socket sc = new Socket();
+		
+		InetSocketAddress isa = new InetSocketAddress("localhost", 8080);
+		System.out.println("client : tentative de connexion");
+		sc.connect(isa);
+		
+		InputStream is = sc.getInputStream();
+		int x = is.read();
+		
+		System.out.println("client : x = "+(char)x);
+		
+		OutputStream os = sc.getOutputStream();
+		os.write(x+1);
+		
+		sc.close();
+		
 	}
 
 }
